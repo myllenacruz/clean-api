b86ba988ad1999fb113c788dd4df090ae52de2dd
Myllena Cruz <myllena.ocruz@gmail.com>
2023-03-07 13:09:34 -0300

    change jest-mongo-db to ts

---
+++ jest-mongodb-config.ts
@@ -0,0 +1,15 @@
+export default {
+	mongodbMemoryServer: {
+		version: "latest"
+	  },
+	  mongodbMemoryServerOptions: {
+		instance: {
+			  dbName: "jest"
+		},
+		binary: {
+			  version: "6.0.4",
+			  skipMD5: true
+		},
+		autoStart: false
+	}
+};
