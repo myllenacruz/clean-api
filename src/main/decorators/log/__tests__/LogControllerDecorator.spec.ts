455e941bd12f0422af8f0d2471d5fe37dbdd273a
Myllena Cruz <myllena.ocruz@gmail.com>
2023-03-08 18:10:23 -0300

    move sut creation (logControllerDecorator) to a factory method

--- src/main/decorators/log/__tests__/LogControllerDecorator.spec.ts
+++ src/main/decorators/log/__tests__/LogControllerDecorator.spec.ts
@@ -3,26 +3,44 @@ import { IController } from "@presentation/protocols/controllers/IController";
 import { IHttpRequest } from "@presentation/protocols/http/IHttpRequest";
 import { IHttpResponse } from "@presentation/protocols/http/IHttpResponse";
 
-class Controller implements IController {
-	async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-		const httpResponse: IHttpResponse = {
-			statusCode: 200,
-			body: {
-				username: "janeDoe",
-				email: "janedoe@email.com",
-				password: "1234"
-			}
-		};
+interface ISystemUnderTest {
+	systemUnderTest: LogControllerDecorator;
+	controller: IController;
+}
 
-		return new Promise(resolve => resolve(httpResponse));
+function makeController(): IController {
+	class Controller implements IController {
+		async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+			const httpResponse: IHttpResponse = {
+				statusCode: 200,
+				body: {
+					username: "janeDoe",
+					email: "janedoe@email.com",
+					password: "1234"
+				}
+			};
+
+			return new Promise(resolve => resolve(httpResponse));
+		}
 	}
+
+	return new Controller();
+}
+
+function makeSystemUnderTest(): ISystemUnderTest {
+	const controller = makeController();
+	const systemUnderTest = new LogControllerDecorator(controller);
+
+	return {
+		systemUnderTest,
+		controller
+	};
 }
 
 describe("LogControllerDecorator", () => {
 	test("Should call controller handle", async () => {
-		const controller = new Controller();
+		const { systemUnderTest, controller } = makeSystemUnderTest();
 		const handleSpy = jest.spyOn(controller, "handle");
-		const systemUnderTest = new LogControllerDecorator(controller);
 		const httpRequest = {
 			body: {
 				username: "janeDoe",
