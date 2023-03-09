f5e7ddc05ade5d00f9a8e4d2fa3f64dbe4952a85
Myllena Cruz <myllena.ocruz@gmail.com>
2023-03-08 22:18:53 -0300

    ensure logControllerDecorator call logErrorRepository if controller returns a server error

--- src/main/decorators/log/LogControllerDecorator.ts
+++ src/main/decorators/log/LogControllerDecorator.ts
@@ -1,16 +1,26 @@
 import { IController } from "@presentation/protocols/controllers/IController";
 import { IHttpRequest } from "@presentation/protocols/http/IHttpRequest";
 import { IHttpResponse } from "@presentation/protocols/http/IHttpResponse";
+import { ILogErrorRepository } from "@data/protocols/log/ILogErrorRepository";
 
 export class LogControllerDecorator implements IController {
 	private readonly controller: IController;
+	private readonly logErrorRepository?: ILogErrorRepository;
 
-	constructor(controller: IController) {
+	constructor(
+		controller: IController,
+		logErrorRepository?: ILogErrorRepository
+	) {
 		this.controller = controller;
+		this.logErrorRepository = logErrorRepository;
 	}
 
 	public async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
 		const httpResponse = await this.controller.handle(httpRequest);
+
+		if (httpResponse.statusCode === 500)
+			await this.logErrorRepository?.log(httpResponse.body.stack);
+
 		return httpResponse;
 	}
 }
--- src/main/decorators/log/__tests__/LogControllerDecorator.spec.ts
+++ src/main/decorators/log/__tests__/LogControllerDecorator.spec.ts
@@ -2,10 +2,13 @@ import { LogControllerDecorator } from "@main/decorators/log/LogControllerDecora
 import { IController } from "@presentation/protocols/controllers/IController";
 import { IHttpRequest } from "@presentation/protocols/http/IHttpRequest";
 import { IHttpResponse } from "@presentation/protocols/http/IHttpResponse";
+import { HttResponse } from "@presentation/helpers/HttpResponse";
+import { ILogErrorRepository } from "@data/protocols/log/ILogErrorRepository";
 
 interface ISystemUnderTest {
 	systemUnderTest: LogControllerDecorator;
 	controller: IController;
+	logErrorRepository: ILogErrorRepository;
 }
 
 function makeController(): IController {
@@ -27,13 +30,25 @@ function makeController(): IController {
 	return new Controller();
 }
 
+function makeLogErrorRepository(): ILogErrorRepository {
+	class LogErrorRepository implements ILogErrorRepository {
+		async log(stack: string): Promise<void> {
+			return new Promise(resolve => resolve());
+		}
+	}
+
+	return new LogErrorRepository();
+}
+
 function makeSystemUnderTest(): ISystemUnderTest {
 	const controller = makeController();
-	const systemUnderTest = new LogControllerDecorator(controller);
+	const logErrorRepository = makeLogErrorRepository();
+	const systemUnderTest = new LogControllerDecorator(controller, logErrorRepository);
 
 	return {
 		systemUnderTest,
-		controller
+		controller,
+		logErrorRepository
 	};
 }
 
@@ -76,5 +91,38 @@ describe("LogControllerDecorator", () => {
 				password: "1234"
 			}
 		});
+	});
+
+	test("Should call LogErrorRepository with correct error if controller returns a server error", async () => {
+		const {
+			systemUnderTest,
+			controller,
+			logErrorRepository
+		} = makeSystemUnderTest();
+
+		const httpRequest = {
+			body: {
+				username: "janeDoe",
+				email: "janedoe@email.com",
+				password: "1234",
+				passwordConfirmation: "1234"
+			}
+		};
+
+		const fakeError = new Error();
+
+		fakeError.stack = "any";
+
+		const error = HttResponse.serverError(fakeError);
+
+		const logSpy = jest.spyOn(logErrorRepository, "log");
+
+		jest.spyOn(controller, "handle").mockReturnValueOnce(
+			new Promise(resolve => resolve(error))
+		);
+
+		await systemUnderTest.handle(httpRequest);
+
+		expect(logSpy).toHaveBeenCalledWith("any");
 	});
 });
