3f4c49bb2b1c5ba4997de52a7c440a7721edac27
Myllena Cruz <myllena.ocruz@gmail.com>
2023-03-10 12:53:07 -0300

    feature: ensure expressRoutesAdapter returns the error message instead of the error name

--- src/main/adapters/express/ExpressAdapter.ts
+++ src/main/adapters/express/ExpressAdapter.ts
@@ -5,13 +5,17 @@ import { IHttpRequest } from "@presentation/protocols/http/IHttpRequest";
 export class ExpressAdapter {
 	public static route(controller: IController) {
 		return async (request: Request, response: Response) => {
-			const httpRequest: IHttpRequest = {
+			const httpResponse = await controller.handle({
 				body: request.body
-			};
+			});
 
-			const httpResponse = await controller.handle(httpRequest);
+			if (httpResponse.statusCode == 200)
+				return response.status(httpResponse.statusCode).json(httpResponse.body);
 
-			return response.status(httpResponse.statusCode).json(httpResponse.body);
+			else
+				return response.status(httpResponse.statusCode).json({ error:
+					httpResponse.body.message
+				});
 		};
 	}
 }
