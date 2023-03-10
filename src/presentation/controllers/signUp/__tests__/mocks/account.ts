081a6dc0bdca3a1d2064e408caa21b2e676cb23d
Myllena Cruz <myllena.ocruz@gmail.com>
2023-03-10 11:23:53 -0300

    add signUpController mocks

---
+++ src/presentation/controllers/signUp/__tests__/mocks/account.ts
@@ -0,0 +1,8 @@
+import { IAccountModel } from "@domain/models/account/IAccountModel";
+
+export const accountModel: IAccountModel = {
+	id: "validId",
+	username: "janeDoe",
+	email: "janedoe@email.com",
+	password: "1234"
+};
