5296c4c5b20dddf6257424efec1699dcef27e7e9
Myllena Cruz <myllena.ocruz@gmail.com>
2023-03-20 18:01:47 -0300

    ensure requiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validation/fields/__tests__/RequiredFieldValidation.spec.ts
+++ src/presentation/helpers/validation/fields/__tests__/RequiredFieldValidation.spec.ts
@@ -3,10 +3,18 @@ import { MissingParamError } from "@presentation/errors/MissingParamError";
 
 describe("RequiredFieldValidation", () => {
 	test("Should return a MissingParamError if validation fails", () => {
-		const systemUnderTest = new RequiredFieldValidation("test");
+		const systemUnderTest = new RequiredFieldValidation("field");
 
 		const error = systemUnderTest.validate({ username: "jane" });
 
-		expect(error).toEqual(new MissingParamError("test"));
+		expect(error).toEqual(new MissingParamError("field"));
+	});
+
+	test("Should not return if validation success", () => {
+		const systemUnderTest = new RequiredFieldValidation("field");
+
+		const error = systemUnderTest.validate({ field: "jane" });
+
+		expect(error).toBeFalsy();
 	});
 });
