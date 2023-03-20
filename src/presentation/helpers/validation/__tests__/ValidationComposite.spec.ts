0e933fec4ba5aad83b7cc50d6edf581369ba7fba
Myllena Cruz <myllena.ocruz@gmail.com>
2023-03-20 18:31:04 -0300

    validationComposite: move sut creation to a factory method

--- src/presentation/helpers/validation/__tests__/ValidationComposite.spec.ts
+++ src/presentation/helpers/validation/__tests__/ValidationComposite.spec.ts
@@ -3,20 +3,37 @@ import { MissingParamError } from "@presentation/errors/MissingParamError";
 import { IValidation } from "@presentation/helpers/validation/IValidation";
 import { IInput } from "@presentation/helpers/validation/interfaces/IInput";
 
+interface ISystemUnderTest {
+	systemUnderTest: ValidationComposite;
+	validation: IValidation;
+}
+
 function makeValidation(): IValidation {
 	class Validation implements IValidation {
 		validate(input: IInput): Error | null {
-			return new MissingParamError("field");
+			return null;
 		}
 	}
 
 	return new Validation();
 }
 
+function makeSystemUnderTest(): ISystemUnderTest {
+	const validation = makeValidation();
+	const systemUnderTest = new ValidationComposite([validation]);
+
+	return {
+		validation,
+		systemUnderTest
+	};
+}
+
 describe("ValidationComposite", () => {
 	test("Should return an error if any validation fails", () => {
-		const validation = makeValidation();
-		const systemUnderTest = new ValidationComposite([validation]);
+		const { systemUnderTest, validation } = makeSystemUnderTest();
+
+		jest.spyOn(validation, "validate").mockReturnValue(new MissingParamError("field"));
+
 		const error = systemUnderTest.validate({ field: "anyValue" });
 
 		expect(error).toEqual(new MissingParamError("field"));
